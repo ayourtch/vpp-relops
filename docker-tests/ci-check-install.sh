94499fddf84fb88a885eddd08b9730603c90f00f
Andrew Yourtchenko <ayourtch@cisco.com>
2025-02-26 19:36:35 +0100

    final check 25.02 release

--- docker-tests/ci-check-install.sh
+++ docker-tests/ci-check-install.sh
@@ -4,4 +4,8 @@ set -eux
 # This file is executed from github actions (aarch64 and x86_64) to check the install on the default packagelist (x86_64) or default-arm (aarch64).
 # If you override VPP_PACKAGE_LIST, it will be attempted on both platforms - which is probably not what you want.
 
-PACKAGECLOUD_REPO=fdio/2502 VPP_EXACT_VERSION=25.02-release ./run-docker-test
+# initial release check
+# PACKAGECLOUD_REPO=fdio/2502 VPP_EXACT_VERSION=25.02-release ./run-docker-test
+
+# post-publish final release check
+PACKAGECLOUD_REPO=fdio/release VPP_CHECK_VERSION=25.02-release ./run-docker-test
